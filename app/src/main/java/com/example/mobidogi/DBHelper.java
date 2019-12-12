9cb7e079f161dd203050b172fac0ceb51bcad70a
ITStudentFromFinland <ihanokvitsiniekka@gmail.com>
2019-12-13 01:43:10 +0200

    Deleted 2 useless buttons from database and finishing touches of database activity

--- app/src/main/java/com/example/mobidogi/DBHelper.java
+++ app/src/main/java/com/example/mobidogi/DBHelper.java
@@ -51,48 +51,6 @@ public class DBHelper extends SQLiteOpenHelper {
     SQLiteDatabase db = this.getWritableDatabase();
     db.insert(TABLE_NAME, null, values);
     db.close();
-  }
-  public DisplayEvent findHandler(String eventdescription) {
-    String query = "Select * FROM " + TABLE_NAME + " WHERE " + COLUMN_DESCRIPTION + " = " + eventdescription + "'";
-    SQLiteDatabase db = this.getWritableDatabase();
-    Cursor cursor = db.rawQuery(query, null);
-    DisplayEvent event = new DisplayEvent();
-    if(cursor.moveToFirst()) {
-      cursor.moveToFirst();
-      event.setDate(Integer.parseInt(cursor.getString(0)));
-      event.setEventDescription(cursor.getString(1));
-      cursor.close();
-    } else {
-      event = null;
     }
-    db.close();
-    return event;
-    }
-
-  public boolean deleteHandler(int Date) {
-    boolean result = false;
-    String query = "Select*FROM " + TABLE_NAME + " WHERE " + COLUMN_DATE + " = '" + Date + "'";
-    SQLiteDatabase db = this.getWritableDatabase();
-    Cursor cursor = db.rawQuery(query, null);
-    DisplayEvent event = new DisplayEvent();
-    if(cursor.moveToFirst()) {
-      event.setDate(Integer.parseInt(cursor.getString(0)));
-      db.delete(TABLE_NAME, COLUMN_DATE + "=?", new String[] {
-        String.valueOf(event.getDate())
-      });
-      cursor.close();
-      result = true;
-    }
-    db.close();
-    return result;
   }
-  public boolean updateHandler(int date, String description) {
-    SQLiteDatabase db = this.getWritableDatabase();
-    ContentValues args = new ContentValues();
-    args.put(COLUMN_DATE, date);
-    args.put(COLUMN_DESCRIPTION, description);
-    return db.update(TABLE_NAME, args, COLUMN_DATE + "=" + date, null) > 0;
-
-  }
-}
 
--- app/src/main/java/com/example/mobidogi/Treenipaivakirja.java
+++ app/src/main/java/com/example/mobidogi/Treenipaivakirja.java
@@ -2,12 +2,10 @@ package com.example.mobidogi;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.text.method.ScrollingMovementMethod;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import static com.example.mobidogi.DBHelper.COLUMN_DATE;
-import static com.example.mobidogi.DBHelper.COLUMN_DESCRIPTION;
 
 
 public class Treenipaivakirja extends AppCompatActivity {
@@ -22,12 +20,13 @@ public class Treenipaivakirja extends AppCompatActivity {
     setContentView(R.layout.treenipaivakirja_activity);
 
     lst = (TextView)findViewById(R.id.list);
+    lst.setMovementMethod(new ScrollingMovementMethod());
     eventdate = (EditText)findViewById(R.id.eventDate);
     eventdescription = (EditText)findViewById(R.id.eventDescription);
   }
 
   public void loadEvents(View view) {
-    DBHelper dbHandler = new DBHelper(this, COLUMN_DATE, null, 1);
+    DBHelper dbHandler = new DBHelper(this, null, null, 1);
     lst.setText(dbHandler.loadHandler());
     eventdate.setText("");
     eventdescription.setText("");
@@ -41,32 +40,7 @@ public class Treenipaivakirja extends AppCompatActivity {
     dbHandler.addHandler(event);
     eventdate.setText("");
     eventdescription.setText("");
-  }
-
-  public void findEvents(View view) {
-    DBHelper dbHandler = new DBHelper(this, null, null, 1);
-    DisplayEvent event = dbHandler.findHandler(eventdescription.getText().toString());
-    if (event != null) {
-      lst.setText(String.valueOf(event.getDate()) + " " + event.getEventDescription());
-
-      } else {
-      lst.setText("Ei löydy");
-      eventdate.setText("");
-      eventdescription.setText("");
     }
   }
-
-  public void updateEvent(View view) {
-    DBHelper dbHandler = new DBHelper(this, null, null, 1);
-    boolean result = dbHandler.updateHandler(Integer.parseInt(
-      eventdate.getText().toString()), eventdescription.getText().toString());
-    if (result) {
-      eventdate.setText("");
-      eventdescription.setText("");
-      lst.setText("Merkintä päivitetty");
-    } else
-      eventdate.setText("Ei löydy");
-  }
-}
 
 
