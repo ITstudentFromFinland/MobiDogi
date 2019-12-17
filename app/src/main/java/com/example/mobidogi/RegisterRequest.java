399522e2e9baa51f5ba860e11b06638cb00204d9
ITStudentFromFinland <ihanokvitsiniekka@gmail.com>
2019-12-17 22:19:35 +0200

    mixed codes up

--- app/src/main/java/com/example/mobidogi/RegisterRequest.java
+++ app/src/main/java/com/example/mobidogi/RegisterRequest.java
@@ -10,13 +10,13 @@ public class RegisterRequest extends StringRequest {
   private static final String REGISTER_REQUEST_URL = "http://unaccountable-fiber.000webhostapp.com/Register.php";
   private Map<String, String> params;
 
-  public RegisterRequest(String name, String username, int age, String password, String dogname, String rotu, Response.Listener<String> listener) {
+  public RegisterRequest(String name, String username, String password, int age, String dogname, String rotu, Response.Listener<String> listener) {
     super(Method.POST, REGISTER_REQUEST_URL, listener, null);
     params = new HashMap<>();
     params.put("name", name);
-    params.put("age", age + "");
     params.put("username", username);
     params.put("password", password);
+    params.put("age", age + "");
     params.put("dogname", dogname);
     params.put("rotu", rotu);
   }
