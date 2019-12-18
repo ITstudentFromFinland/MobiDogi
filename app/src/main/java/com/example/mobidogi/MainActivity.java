38f552440388d3b83c4d2754ec830bb0080ec295
ITStudentFromFinland <ihanokvitsiniekka@gmail.com>
2019-12-18 23:46:33 +0200

    updated the permission for Google Play Billing

--- app/src/main/java/com/example/mobidogi/MainActivity.java
+++ app/src/main/java/com/example/mobidogi/MainActivity.java
@@ -3,9 +3,7 @@ package com.example.mobidogi;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.BottomNavigationView;
-import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
@@ -36,6 +34,7 @@ public class MainActivity extends AppCompatActivity {
           case R.id.navigation_dashboard:
             Intent y = new Intent(MainActivity.this, YhteysKouluttajaanActivity.class);
             startActivity(y);
+
             break;
 
           case R.id.navigation_notifications:
